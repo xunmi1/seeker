2e640a1356c6cea302ccd41c888bebf83f25273b
xunmi <xunmi1@outlook.com>
2019-03-24 21:35:30 +0800

    build(build): 修复构建错误

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,6 +13,6 @@ git commit -m 'deploy Seeker'
 git config --local user.name "xunmi"
 git config --local user.email "xunmi1@outlook.com"
 
-git push -f git@github.com/xunmi1/seeker.git master:gh-pages
+git push -f git@github.com:xunmi1/seeker.git master:gh-pages
 
 cd -
