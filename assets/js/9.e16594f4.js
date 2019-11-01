e5aa1574777f25da736f7558f800f49412bcdb8b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-01 13:18:30 +0000

    Deploy xunmi1/seeker to github.com/xunmi1/seeker.git:gh-pages

---
+++ assets/js/9.e16594f4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{58:function(n,w,o){}}]);
--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -21,51 +27,51 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "be39c26955a1b455014569b4121598d1"
+    "revision": "894ca91169e5f54a8a3cd03d531d20bd"
   },
   {
-    "url": "assets/css/0.styles.9759a56c.css",
-    "revision": "887f6c69dff88044fabe17e34eecf339"
+    "url": "assets/css/0.styles.8d4168b3.css",
+    "revision": "e2ede15365bf941b57fea1c4e9143e96"
   },
   {
     "url": "assets/img/search.83621669.svg",
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/2.93c59367.js",
-    "revision": "1b8a1a3c8e2b6bfc4ab2b70c3cd0afea"
+    "url": "assets/js/2.4b4116de.js",
+    "revision": "271a675dac968ae4993a4b8327188fc9"
   },
   {
-    "url": "assets/js/3.92dac5eb.js",
-    "revision": "68697e5e8cf5aadd633f55327a5725ff"
+    "url": "assets/js/3.3e908569.js",
+    "revision": "014818f88a198938802f3ac3e934b9c8"
   },
   {
-    "url": "assets/js/4.2c3748b4.js",
-    "revision": "4797bfe29808a66d845ae7d3792cebaa"
+    "url": "assets/js/4.aa54ef99.js",
+    "revision": "4610dd51de3bfbaeaaf68c2b424ca200"
   },
   {
-    "url": "assets/js/5.d804be93.js",
-    "revision": "e89a81e6a608d3bd9b10854442119b03"
+    "url": "assets/js/5.471c19ca.js",
+    "revision": "4cb57eb5136e5704ed782ad8dba78263"
   },
   {
-    "url": "assets/js/6.39dfed6d.js",
-    "revision": "6ea097c9017c5cd3d337eb715f97825b"
+    "url": "assets/js/6.e4edb209.js",
+    "revision": "1f43e92ce53757be8e8f1a438b5ee734"
   },
   {
-    "url": "assets/js/7.bed7e6c5.js",
-    "revision": "a61e2a1e77b8b9a0a43c61117ac0a3b5"
+    "url": "assets/js/7.4dfb6a1b.js",
+    "revision": "5f6ebba74f3fa357950741aea204a44c"
   },
   {
-    "url": "assets/js/8.b5ac5326.js",
-    "revision": "901903c88b0a583cdbdda8e653e8d86f"
+    "url": "assets/js/8.12c279bf.js",
+    "revision": "2dcc980c21b287541b7ccbb11acc2e60"
   },
   {
-    "url": "assets/js/9.d67efb76.js",
-    "revision": "5505510e24dc88bdbfdbcccffd2f4195"
+    "url": "assets/js/9.e16594f4.js",
+    "revision": "e8b86536eff7f27c4565d40df0a559c7"
   },
   {
-    "url": "assets/js/app.38716c7e.js",
-    "revision": "36fcfa7f2fdeedbf7e6d8ecd16393cc3"
+    "url": "assets/js/app.58e1484c.js",
+    "revision": "4e378ec69c2ae7c97bbb503741e2ff32"
   },
   {
     "url": "icons/android-chrome-192x192.png",
@@ -77,26 +83,25 @@ self.__precacheManifest = [
   },
   {
     "url": "icons/fire.svg",
-    "revision": "9c5040fd6fb239bf9b847ed51ef862a2"
+    "revision": "840d88859e56cd85977ad35b5a1bba00"
   },
   {
     "url": "index.html",
-    "revision": "7449046e933c47beebc3fc07cb9563e0"
+    "revision": "5c6be0830a18d182e83424492c0b1a5e"
   },
   {
     "url": "others/fe-style.html",
-    "revision": "7833012107ff4afde899ad064fb9b89e"
+    "revision": "a0cc4fbb7bf6344d9084ac80fda61fcf"
   },
   {
     "url": "others/git-summary.html",
-    "revision": "d6cb45be486413c6a4798c94b9ef9f10"
+    "revision": "8f129120b74e76e8c96dd698bdaeae47"
   },
   {
     "url": "user.png",
     "revision": "daaa1a090c3543607a574b685a05b81d"
   }
 ].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 addEventListener('message', event => {
   const replyPort = event.ports[0]
