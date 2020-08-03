c8d196d4623fa767dc77ade87569b8236ffb34f9
xunmi1 <xunmi1@users.noreply.github.com>
2020-08-03 09:31:35 +0000

    deploy: 1755177dae4165a5e6dd1d1c68e73f75ee412aa3

--- service-worker.js
+++ service-worker.js
@@ -27,11 +27,11 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "47c493dedf1650359e4cb4a677948b69"
+    "revision": "0749839ef4e747dfda13e364c32a67a3"
   },
   {
-    "url": "assets/css/0.styles.43ef9606.css",
-    "revision": "0e26d059abed59dafb9a2ab24ecce986"
+    "url": "assets/css/0.styles.cb318ca4.css",
+    "revision": "7d32fd6b117afd35e26b207aaaf26fa2"
   },
   {
     "url": "assets/img/git-operations.14ad858d.png",
@@ -42,40 +42,40 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/2.4909e647.js",
-    "revision": "cc5bebc4952da68980f223e33b22c061"
+    "url": "assets/js/2.38e26469.js",
+    "revision": "214995664642f1549062f2f6682dd282"
   },
   {
-    "url": "assets/js/3.53884e4b.js",
-    "revision": "5708f6da086742b8304a8c0cc6d44a6b"
+    "url": "assets/js/3.0e8d9838.js",
+    "revision": "2d58dcddd2a9e0b3608fb5ba9016ed33"
   },
   {
-    "url": "assets/js/4.9574794f.js",
-    "revision": "5802095112706facd35f67a912626f85"
+    "url": "assets/js/4.b687a8a0.js",
+    "revision": "1586a13816f001fb2bdbab38ca6b238d"
   },
   {
-    "url": "assets/js/5.8ee0982f.js",
-    "revision": "10ad76ed43a9476cafba73c00db99046"
+    "url": "assets/js/5.ce98caab.js",
+    "revision": "c13bc2f959213d7cfb02c850d035a37e"
   },
   {
-    "url": "assets/js/6.c64cb59d.js",
-    "revision": "531dda117e06fd84472c15aa9a540f39"
+    "url": "assets/js/6.1299e300.js",
+    "revision": "9ab1f3d807e9319616883db480eb066b"
   },
   {
-    "url": "assets/js/7.224ad5a6.js",
-    "revision": "7003566be7bc48f31492ebed668ff39a"
+    "url": "assets/js/7.4a17cf70.js",
+    "revision": "94172a0186c420197e97ba214b16089c"
   },
   {
-    "url": "assets/js/8.5e2cd426.js",
-    "revision": "be5c94a3f8129a3bb2b2f288374d36f2"
+    "url": "assets/js/8.6d610868.js",
+    "revision": "54316d8e2e74250328d6c85222e0f1ab"
   },
   {
-    "url": "assets/js/9.ca17a6f5.js",
-    "revision": "e7bba6a6f07f9878544fcf179ae26f6a"
+    "url": "assets/js/9.b3d40b4e.js",
+    "revision": "18eb4613fa43c4fb1e31d7290ed7dec1"
   },
   {
-    "url": "assets/js/app.1e296eff.js",
-    "revision": "9de0ef267d10e9b0be9485a57ae56ed0"
+    "url": "assets/js/app.cb5036c1.js",
+    "revision": "f8e4a86ea0d0eb7bd51c842c33132a61"
   },
   {
     "url": "icons/android-chrome-192x192.png",
@@ -91,15 +91,15 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "d6f1d1acd8939372250cd95445c3bda5"
+    "revision": "8cf65e964991134df0054844ce5ea40e"
   },
   {
     "url": "others/fe-style.html",
-    "revision": "2cfbaf69b9a4f29bf3dda76cfe7f8079"
+    "revision": "b714535b68a719a590b1bfe6fa8d52e3"
   },
   {
     "url": "others/git/git-summary.html",
-    "revision": "1d7d1cc4bbc2d214ca80064622f50768"
+    "revision": "7aebe5e19239e1108385a6e8175ea1d0"
   },
   {
     "url": "user.png",
